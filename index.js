6cc05b3f37fd319b51e57428f6189cf6d4f8bc55
rizkajuliant20 <rizkariz20@gmail.com>
2022-06-04 23:37:18 +0700

    first commit for fourth challenge

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+    "2019-01-02", // 1546387200 (epoch time, dalam detik)
+    "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
+    "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
+    "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
+    "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, index) => {
+    const fusion = []
+    let temp;
+    if (index == undefined) {
+        for (let i = 0; i < dates.length; i++) {
+            fusion.push(new Date(dates[i]).getTime() / 1000)
+        }
+        fusion.sort()
+        for (let n = 0; n < fusion.length; n++) {
+            if (n == 0) {
+                temp = fusion[n]
+            } else {
+                temp = temp + "-" + fusion[n]
+            }
+        }
+        return temp
+    }
+
+    return (new Date(dates[index]).getTime() / 1000).toString()
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
-  // IIFE
+    // IIFE
 
-  // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
-  console.log(createDate?.(dates));
+    // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
+    console.log(createDate ? .(dates));
 
-  // '1614841200' (dalam string)
-  console.log(createDate?.(dates, 2));
+    // '1614841200' (dalam string)
+    console.log(createDate ? .(dates, 2));
 })();
 
 module.exports = {
-  dates,
-  createDate,
+    dates,
+    createDate,
 };
